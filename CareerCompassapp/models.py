82857d8604557086a9d178c587ba19413941694c
Laiba Khan <152780287+Laibaa-Khan@users.noreply.github.com>
2025-10-24 22:46:42 +0500

    Update models.py

--- CareerCompassapp/models.py
+++ CareerCompassapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Internships(models.Model):
+    internship_id = models.AutoField(primary_key=True)
+    company_id = models.IntegerField()
+    title = models.CharField(max_length=255)
+    duration = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    skills_required = models.CharField(max_length=255)
+    deadline = models.DateField()
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
