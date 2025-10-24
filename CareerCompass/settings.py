b03cb2f50b9034140e56ddf11c7d1a9e5564e86e
Laiba Khan <152780287+Laibaa-Khan@users.noreply.github.com>
2025-10-24 22:51:21 +0500

    Update settings.py

--- CareerCompass/settings.py
+++ CareerCompass/settings.py
@@ -59,7 +59,7 @@ WSGI_APPLICATION = 'CareerCompass.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'career_compass.db',
     }
 }
 
